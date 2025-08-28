98e9f910ad250e3df6110548eafb079b37349bb7
hamzakargin <hamzakargin@outlook.com>
2025-08-28 16:30:39 +0300

    Create main.yml

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -52,7 +52,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res, next) => {
     try {
         const db = await connectToDatabase();
-        const collection = db.collection("gifts");
+        const collection = db.collection("giftss");
         const gift = await collection.insertOne(req.body);
 
         res.status(201).json(gift.ops[0]);
